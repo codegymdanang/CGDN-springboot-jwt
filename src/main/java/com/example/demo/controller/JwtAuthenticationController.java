1cdc4c2ce0a258babd06ba0cb2e45087f941913f
Codegym DN <codegymdanang@gmail.com>
2019-07-31 20:36:38 +0700

    add comment

--- src/main/java/com/example/demo/controller/JwtAuthenticationController.java
+++ src/main/java/com/example/demo/controller/JwtAuthenticationController.java
@@ -50,6 +50,7 @@ public class JwtAuthenticationController {
         return ResponseEntity.ok(new JwtResponse(token));
     }
 
+
     private void authenticate(String username, String password) throws Exception {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
